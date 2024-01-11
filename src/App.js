1cc655e9c5f52b958e33b0750206c762dfd9ad82
anvarjon-programmer <mranvarjontojiboyevgmail.com>
2024-01-12 00:14:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Contries } from './components/Contries';
+import { Navbar } from './components/Navbar';
+import Users from './components/Users';
+import { Home } from './components/Home';
+import { Card } from 'reactstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+        <Route path='/users' element={<Users/>}/>
+        <Route path='/contries' element={ <Contries/>}/>
+        <Route path='/card' element={ <Card/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export const Navbar = () => {
+  return (
+    <div className="container">
+      <div className='navbar'>
+        <h1 className='lo'>lo<i className='go'>go</i></h1>
+        <div className='links'>
+         {/* <NavLink className='btn btn-success' to='/'>Home</NavLink> */}
+         <NavLink className='btn btn-success' to='/Users'>Users Info</NavLink>
+         <NavLink className='btn btn-success' to='/Contries'>Contries</NavLink>
+         <NavLink className='btn btn-success' to='/Card'>Card</NavLink>
+        </div>
+      </div>
+    </div>
+  )
+}
