2c5365cd8e74a126b24f4d063cb01737409ebfae
anvarjon-programmer <mranvarjontojiboyevgmail.com>
2024-01-12 01:47:30 +0300

    card componenttiga pagination qo'shildi

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,17 +3,23 @@ import React, { useEffect, useState } from 'react'
 
 export const Cards = () => {
     const [albums,setAlbums] = useState([]);
+    const [currentPage,setCurrentPage] = useState(1);
+    const [itemsPerPage,setitemsPerPage] = useState(100);
     useEffect(() =>{
         axios.get("https://jsonplaceholder.typicode.com/photos").then(response=>{
             setAlbums(response.data);
-            console.log(response.data);
         })
     },[]);
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentCountries = albums.slice(indexOfFirstItem,indexOfLastItem);
+
+    const paginate = pageNumber => setCurrentPage(pageNumber)
   return (
     <div className='container'>
         <div className="row">
             {
-                albums.map((item,index) =>{
+                currentCountries.map((item,index) =>{
                     return <div className="col-lg-3">
                         <div className="card">
                         <img className='card-img-top' src={item.url} alt="" />
@@ -21,6 +27,13 @@ export const Cards = () => {
                     </div>
                 })
             }
+        </div>
+        <div className="paginationAlbums">
+        {[...Array(Math.ceil(albums.length / itemsPerPage)).keys()].map(number => (
+                    <button className='btn btn-primary' key={number + 1} onClick={() => paginate(number + 1)}>
+                        {number + 1}
+                    </button>
+                ))}
         </div>
     </div>
   )
